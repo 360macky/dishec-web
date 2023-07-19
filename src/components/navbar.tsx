a49b1f080f58453175c7a8b7e7483b4132023168
Marcelo Arias <hello@marceloarias.com>
2023-07-18 23:14:40 -0500

    Add icons and logos

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,9 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
-        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="https://www.dishec.com">Dishec</a>
+        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="https://www.dishec.com">
+          <img src="./logo.png" alt="" width={120} />
+        </a>
         <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon mt-2">
             <span className="navbar-toggler-bar bar1"></span>
@@ -34,12 +36,12 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="https://github.com/creativetimofficial/astro-ecommerce">
-                <i className="fab text-lg fa-github"></i>
+                <i className="fab text-lg fa-facebook"></i>
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="https://discord.com/invite/TGZqBvZB" rel="nofollow" target="_blank">
-                <i className="fab text-lg fa-discord"></i>
+                <i className="fab text-lg fa-whatsapp"></i>
               </a>
             </li>
           </ul>
